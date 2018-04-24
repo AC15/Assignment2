e45b261eb8a6df81cf7d77b8fce6865883fff041
AC15 <aleksander.czarnowski@gmail.com>
2018-04-24 17:33:30 +0100

    Removed NetBeans project files. Added a menu bar with an about menu.

--- src/clock/View.java
+++ src/clock/View.java
@@ -7,46 +7,57 @@ import java.util.Observable;
 
 public class View implements Observer {
     
-    ClockPanel panel;
-    
+    private ClockPanel panel;
+    private JFrame frame;
+
     public View(Model model) {
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         panel = new ClockPanel(model);
         //frame.setContentPane(panel);
         frame.setTitle("Java Clock");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        // Start of border layout code
-        
-        // I've just put a single button in each of the border positions:
-        // PAGE_START (i.e. top), PAGE_END (bottom), LINE_START (left) and
-        // LINE_END (right). You can omit any of these, or replace the button
-        // with something else like a label or a menu bar. Or maybe you can
-        // figure out how to pack more than one thing into one of those
-        // positions. This is the very simplest border layout possible, just
-        // to help you get started.
-        
+
         Container pane = frame.getContentPane();
-        
-        JButton button = new JButton("Button 1 (PAGE_START)");
-        pane.add(button, BorderLayout.PAGE_START);
-         
+
         panel.setPreferredSize(new Dimension(200, 200));
         pane.add(panel, BorderLayout.CENTER);
-         
-        button = new JButton("Button 3 (LINE_START)");
-        pane.add(button, BorderLayout.LINE_START);
-         
-        button = new JButton("Long-Named Button 4 (PAGE_END)");
-        pane.add(button, BorderLayout.PAGE_END);
-         
-        button = new JButton("5 (LINE_END)");
-        pane.add(button, BorderLayout.LINE_END);
-        
-        // End of borderlayout code
-        
+
+        addMenuBar();
+
         frame.pack();
         frame.setVisible(true);
+    }
+
+    /**
+     * Adds a menu bar to the top of the program.
+     */
+    private void addMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+
+        addAboutMenu(menuBar);
+    }
+
+    /**
+     * Adds an About menu to the menu bar.
+     * Once clicked it displays a dialog box with author's name.
+     *
+     * @param menuBar Menu bar to which the about menu will be attached.
+     */
+    private void addAboutMenu(JMenuBar menuBar) {
+        JMenu aboutMenu = new JMenu("About");
+        aboutMenu.setMnemonic('a');
+
+        JMenuItem aboutItem = new JMenuItem("About");
+        aboutItem.setMnemonic('a');
+        aboutItem.addActionListener(e ->
+                JOptionPane.showMessageDialog(null,
+                        "Java clock by Aleksander Czarnowski",
+                        "About", JOptionPane.INFORMATION_MESSAGE)
+        );
+        aboutMenu.add(aboutItem);
+
+        menuBar.add(aboutMenu);
     }
     
     public void update(Observable o, Object arg) {
